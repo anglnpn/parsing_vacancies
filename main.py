3c3f240aca465bbc01369c018569f1c3a36ecdf8
angelina <angln.pnn@gmail.com>
2023-09-19 22:04:56 -0300

    Изменили функцию user_interaction() для работы с пользователем, убрали двоение кода

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
-
-from classes import SuperJobAPI, HeadHunterAPI, JSONFileManager
+# Импорт классов
+from classes import SuperJobAPI, HeadHunterAPI, JSONFileManager, Vacancy
 
 
 def user_interaction():
     """
-    Функция для работы с пользователем
-    :return:
+    Функция для работы с пользователем.
+    Получает ввод данных
+    Возвращает ответ в виде файла с вакансиями.
+    Сортирует их по заданным критериям.
+
     """
+
     town = input("Введите город: ").title()
     search_query = input("Введите поисковый запрос: ").title()
-    platform = input('Выберите платформу для поиска вакансий: "HeadHunter", "SuperJob" ')
-    if platform == "HeadHunter":
-        job_api = HeadHunterAPI(town, search_query)
-        unknown_file = job_api.get_vacancies()
-        js_file = JSONFileManager()
-        js_file.write(unknown_file)
-        js_file.read()
-        # print(job_api.get_vacancies())
-    elif platform == "SuperJob":
-        job_api = SuperJobAPI(town, search_query)
-        unknown_file = job_api.get_vacancies()
-        js_file = JSONFileManager()
-        js_file.write(unknown_file)
-        js_file.read()
-        # print(job_api.get_vacancies())
+    platform = input('Выберите платформу для поиска вакансий: "HeadHunter", "SuperJob"\n'
+                     'Введите: "H" или "S"  \n').title()
 
-    # top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    # filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    # filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
+    # Присваиваем переменной имя класса
+    if platform == "H":
+        platform_job = HeadHunterAPI
+    else:
+        platform_job = SuperJobAPI
 
-    # if not filtered_vacancies:
-    #     print("Нет вакансий, соответствующих заданным критериям.")
-    #     return
+    # создаем экземпляр класса
+    job_api = platform_job(town, search_query)
+    # вызываем метод для парсинга данных по api
+    vacancies = job_api.parse_vacancies()
+    # вызываем метод для форматирования вакансий по определенному шаблону
+    vacancies_format = job_api.formatting_vacancies(vacancies)
+    # создаем экземпляр класса для работы с файлом
+    js_file = JSONFileManager()
+    # вызываем метод форматирования
+    js_file.write(vacancies_format)
+    # читаем файл
+    file_vac = js_file.read()
+    print(file_vac)
 
-    # sorted_vacancies = sort_vacancies(filtered_vacancies)
-    # top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    # print_vacancies(top_vacancies)
+    filter_vacancies = input("Включить фильтрацию вакансий по заработной плате?\n"
+                             "Введите: 'да' или 'нет'  ")
+    if filter_vacancies == 'да':
+        # создаем экземпляр класса для работы с вакансиями
+        vacancy_ = Vacancy(file_vac)
+        range_amount = input("Сортировать по возрастанию или убыванию?\n"
+                             "Введите; 'в' или 'у'  ")
+        if range_amount == 'в':
+            # вызываем метод для сортировки по убыванию
+            print(vacancy_.sorted_salary_min())
+        elif range_amount == 'у':
+            # вызываем метод для сортировки по возрастанию
+            print(vacancy_.sorted_salary_max())
 
 
-user_interaction()
+if __name__ == "__main__":
+    user_interaction()
 
-# print(superjob_api)
-# print(hh_api)
-#
-# job_api.get_vacancies()
-# job_api.get_vacancies()
+
